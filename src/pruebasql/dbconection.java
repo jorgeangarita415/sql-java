56c3ff9c4125aff71ff32cab968ae21d5772a3a9
Jorge <jaeangarita@gmail.com>
2025-07-03 19:13:59 -0500

    Primera version del codigo de coneccion

---
+++ src/pruebasql/dbconection.java
@@ -0,0 +1,25 @@
+package pruebasql;
+
+import java.sql.*;
+
+/**
+ *
+ * @author jade
+ */
+public class dbconection {
+    static String url = "jdbc:mysql://localhost:3306/sis_ventas";  // <-- Corregido "lochost" a "localhost"
+    static String user = "root";
+    static String pass = "";
+
+    public static Connection conectar() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Conexion exitosa");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+        }
+
+        return con;
+    }
+}
